a2283e67f760b48666a95d89ec6711779eaeb45b
lokkochan <chunlok.chan@ucalgary.ca>
2022-11-19 10:51:19 -0700

    Start up with class users

---
+++ App_main.py
@@ -0,0 +1,11 @@
+"""
+Code to change 2022
+RTHbuddy sponsoredbyRTH
+"""
+
+class users:
+    def __init__(self, points, task_history):
+        self.points=points
+        self.task_history=task_history
+        
+    
