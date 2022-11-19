dc27f24865a4297778dbb4a2501bb00c452f96a2
Ka1dri0N <82828627+Ka1dri0N@users.noreply.github.com>
2022-11-19 14:12:36 -0700

    Create qr_gen.py

---
+++ qr_gen.py
@@ -0,0 +1,11 @@
+import qrcode
+
+qr = qrcode.QRCode(
+    version = 1
+    box_size = 16
+    border = 4
+    )
+qr.add_data()
+qr.make(fit = True)
+
+img = qr.make_image(fill_color = 'black', back_color = 'white')
