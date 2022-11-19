8cf0acaa8749d6c0c811244a8977a60b7eade3ed
lokkochan <chunlok.chan@ucalgary.ca>
2022-11-19 10:59:40 -0700

    more basic in

--- App_main.py
+++ App_main.py
@@ -3,9 +3,33 @@ Code to change 2022
 RTHbuddy sponsoredbyRTH
 """
 
+
+
+
 class users:
     def __init__(self, points, task_history):
         self.points=points
         self.task_history=task_history
+
+    def checkpoints(self):
+        print('The current point is : '+self.point+'.')
+
+
+    def checkhist(self):
+
         
-    
+
+
+
+
+
+
+def main():
+
+
+
+
+
+
+
+main()
