621c1389219561c6b3afe7b411204913b0885bf5
iBobbyTS <ibobbyts@gmail.com>
2022-11-19 13:35:33 -0700

    Update toga code

--- hackrth/src/hackrth/app.py
+++ hackrth/src/hackrth/app.py
@@ -16,15 +16,37 @@ class HackRTH(toga.App):
         We then create a main window (with a name matching the app), and
         show the main window.
         """
-        main_box = toga.Box()
-        label = toga.Label('test')
-        button = toga.Button('butn')
-        main_box.add(label)
-        main_box.add(button)
+        main_box = toga.Box(style=Pack(direction=COLUMN, padding=2))
+        view_options = [
+            toga.Button('View Tasks', on_press=self.task_view, style=Pack(padding=3)),
+            toga.Button('View History', on_press=self.history_view, style=Pack(padding=3)),
+            toga.Button('View Points', on_press=self.points_view, style=Pack(padding=3)),
+        ]
+        for options in view_options:
+            main_box.add(options)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
+
+    def task_view(self, widget):
+        new_box = toga.Box()
+        self.main_window.title = 'View Tasks'
+        # path_label = toga.Label(str(toga.app().paths))
+        self.main_window.content = new_box
+
+        # Store file in application storage (android)
+
+
+    def history_view(self, widget):
+        new_box = toga.Box()
+        self.main_window.title = 'View History'
+        self.main_window.content = new_box
+
+    def points_view(self, widget):
+        new_box = toga.Box()
+        self.main_window.title = 'View Points'
+        self.main_window.content = new_box
 
 
 def main():
