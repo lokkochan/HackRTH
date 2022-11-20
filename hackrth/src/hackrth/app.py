01e76fc033b4487f7ebb70bc28d49205598773d4
lokkochan <chunlok.chan@ucalgary.ca>
2022-11-20 11:06:43 -0700

    Update app.py

--- hackrth/src/hackrth/app.py
+++ hackrth/src/hackrth/app.py
@@ -11,29 +11,6 @@ from toga.style.pack import COLUMN, ROW
 from . import refresh_user_menu as rum
 
 
-# def task_menu():
-# time = 6
-# days_passed = 0
-
-# #refresh menu
-# if time != 6:
-#     #wait 1 hour
-#     time += 1
-#     if time == 24:
-#         time = 0
-#         days_passed += 1
-#     else:
-#         rum.print_menu()
-#         #wait 1 hour
-#         time += 1
-# return rum.print_menu()
-
-# def redeem():
-#     '''Will implant to generate a QR code for scanning to redeem the points'''
-#     qr_code = qrg.qr_gen()
-#     return qr_code
-
-
 class HackRTH(toga.App):
     def startup(self):
         """
@@ -49,10 +26,7 @@ class HackRTH(toga.App):
         data_dir = os.path.join(self.paths.data, 'data.json')
         self.data_dir = data_dir
         # Load data and show main view if data exists, else ask for name and create data
-        # print(os.path.exists(data_dir), self.data_file.read())
 
-        # if os.path.exists(data_dir):
-        #     os.remove(data_dir)
         if os.path.exists(data_dir):
             with open(data_dir) as f:
                 self.data = json.load(f)
@@ -77,7 +51,7 @@ class HackRTH(toga.App):
         self.main_window.title = self.formal_name
         self.main_window.content = main_box
 
-    def ask_for_name_view(self):
+    def ask_for_name_view(self,widget):
         self.main_window.title = 'Welcome to HackRTH'
         self.name_input = toga.TextInput(style=Pack(flex=1))
         self.main_window.content = toga.Box(
@@ -203,9 +177,9 @@ class HackRTH(toga.App):
         except:
             new_box.add(toga.Label('Failed to connect to server.'))
         else:
-            self.data['points'] -= points_to_redeem
-            self.data['history'].append(['Redeem', -points_to_redeem])
-            self.save_data()
+        self.data['points'] -= points_to_redeem
+        self.data['history'].append(['Redeem', -points_to_redeem])
+        self.save_data()
         self.main_window.content = new_box
 
     def confirm_reset_view(self, widget):
